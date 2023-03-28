ead1e16b0788fb570cf2e030e63209e6943c701a
subzero <shubham8111994@gmail.com>
2023-03-28 07:24:23 +0530

    Program for unit conversions

---
+++ UnitConverter.js
@@ -0,0 +1,9 @@
+const num = 10;
+//converting foot to inch
+console.log(num * 12);
+//converting feet to meter
+console.log(num / 3.2808);
+//converting inch to feet
+console.log(num / 12);
+//converting meter to feet
+console.log(num * 3.2808);
