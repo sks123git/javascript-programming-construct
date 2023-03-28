8b7c93b97f7e5f4027532c176f4e23cf4f99ffbe
subzero <shubham8111994@gmail.com>
2023-03-28 07:42:39 +0530

    Program for powers of 2

---
+++ PowersOf2.js
@@ -0,0 +1,12 @@
+let number = 10;
+console.log('powers of two are: ');
+for (
+  let i = 1;
+  i <= number;
+  i++ //checking if i is less than or equal to input number
+)
+  if (i == 1) {
+    console.log(i);
+  } else if (i % 2 == 0) {
+    console.log(i);
+  }
